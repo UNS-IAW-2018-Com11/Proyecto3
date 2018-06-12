c0933583aea9fd17bbfbe2f2c5c532e4d5c1f7a7
Joa <joaco.piersigilli@gmail.com>
2018-06-12 14:47:39 -0300

    Color navbar login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,9 +22,9 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <nav style="	background-color: #02025b;" class="navbar navbar-expand-md navbar-light navbar-laravel" >
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a style="color: white;" class="navbar-brand" href="{{ url('/') }}">
                     Tournament Generator
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -41,8 +41,8 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                            <li><a style="color: white;"class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                            <li><a style="color: white;"class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
