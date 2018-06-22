a9aa82e2ec610e4cafbac4fab1f175e8f1e05001
Joa <joaco.piersigilli@gmail.com>
2018-06-22 16:34:39 -0300

    Changes to youtube

--- public/js/youtubeAPI.js
+++ public/js/youtubeAPI.js
@@ -1,25 +1,40 @@
+var channelName = 'ILoveBasketballTV';
 var vidWidth = '700';
 var vidHeight= '400';
 var results = 5;
 
+
 $(document).ready(function(){
+    $.get(
+        "https://www.googleapis.com/youtube/v3/channels", {
+            part:'contentDetails',
+            forUsername:channelName,
+            key:'AIzaSyBEh9gxvkMkF_SkSQcHNZAIuz2HeWVenvo'},
+            function(data){
+                    $.each(data.items,function(i,item){
+                    var pid = item.contentDetails.relatedPlaylists.uploads;
+                    getVids(pid);
+            });
+        }
+    );
+});
+
+function getVids(pid){
     $.get(
         "https://www.googleapis.com/youtube/v3/playlistItems", {
             part:'snippet',
             fields:'items/snippet(title,resourceId)',
             maxResults:results,
-            playlistId: 'PLanlZeHNuf9QKU_Z-wN4WhqBdIryqGux0',
+            playlistId: pid,
             key:'AIzaSyBEh9gxvkMkF_SkSQcHNZAIuz2HeWVenvo'},
             function(data){
                 var output;
                 $.each(data.items,function(i,item){
                     title = item.snippet.title;
                     videoId = item.snippet.resourceId.videoId;
-                    console.log("title: "+title);
-                    console.log("videoId: "+videoId);
                     output = '<li><iframe height="'+vidHeight+'" width="'+vidWidth+'"src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
                     $('#results').append(output);
                 });
             }
          );
-   });
+   }
