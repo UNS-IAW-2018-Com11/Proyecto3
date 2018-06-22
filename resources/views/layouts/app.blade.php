5ebfd8d871972bfe1e174710995cf022d7bb8b10
Joa <joaco.piersigilli@gmail.com>
2018-06-22 19:46:54 -0300

    changes to login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,50 +22,62 @@
 </head>
 <body>
     <div id="app">
-        <nav style="	background-color: #02025b;" class="navbar navbar-expand-md navbar-light navbar-laravel" >
-            <div class="container">
-                <a style="color: white;" class="navbar-brand" href="{{ url('/') }}">
-                    Tournament Generator
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+      <nav class="navbar  navbar-expand-lg fixed-top">
+        <a class="navbar-brand" href=""> <img
+          src="{{asset('images/liga.png')}}" width="30" height="30" alt="">
+          Tournament Generator
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-                    </ul>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active"><a class="nav-link"
+            href="{{route('index')}}">Home <span class="sr-only">(current)</span>
+          </a></li>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a style="color: white;"class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                            <li><a style="color: white;"class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+          <li class="nav-item"><a class="nav-link" href=""
+            onclick="alert('Tournament Generator IAW 2018 Beta')">About</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{route('contact')}}">Contact</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{route('youtube')}}">Improve your technique!!</a></li>
 
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+          </ul>
+          <button class="btn btn-primary normal-mode" id="toggleButton"
+          type="submit" onclick="toggleMode();">Dark Mode</button>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+
+              <!-- Authentication Links -->
+              @guest
+                  <div class="nav-item"><a style="color: white;"class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></div>
+                  <div class="nav-item"><a style="color: white;"class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></div>
+              @else
+                  <li class="nav-item dropdown">
+                      <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                          {{ Auth::user()->name }} <span class="caret"></span>
+                      </a>
+
+                      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <a class="dropdown-item" href="{{ route('logout') }}"
+                             onclick="event.preventDefault();
+                                           document.getElementById('logout-form').submit();">
+                              {{ __('Logout') }}
+                          </a>
+
+                          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                              @csrf
+                          </form>
+                      </div>
+                  </li>
+              @endguest
+
+
+        </div>
+      </nav>
 
         <main class="py-4">
             @yield('content')
